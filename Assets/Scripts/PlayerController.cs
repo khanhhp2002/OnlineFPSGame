5986e4844d6da2167ff1ce7e401216808ac1b842
khanhbq_HE2002 <khanhbui1560@gmail.com>
2022-12-14 17:29:14 +0700

    Update Weapon

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,20 +8,36 @@ public class PlayerController : MonoBehaviour
     public float mouseSensitivity = 1f;
     private float verticalRotStore;
     private Vector2 mouseInput;
+
     public bool invertLook;
+
     public float movSpeed = 5f, runSpeed = 8f;
     private Vector3 moveDir, movement;
+
     public CharacterController characterController;
+
     private Camera cam;
-    public float jumpForce, gravityMod;
+
+    public float jumpForce, gravityMod = 2.5f;
+
     public Transform groundCheckPoint;
     public LayerMask groundLayers;
+
+    public GameObject bulletImpact;
+    public float timeBetweenShoots = .1f;
+    private float shootCounter;
+
+    public float maxHeat = 10f, heatPerShoot = 1f, coolRate = 4f, overHeatCoolRate = 5f;
+    private float heatCounter;
+    private bool overHeat;
+
     // Start is called before the first frame update
     void Start()
     {
         // lock cursor on the center of the screen and disable it
         Cursor.lockState = CursorLockMode.Locked;
         cam = Camera.main;
+        UIController.instance.weaponHeatSlider.maxValue = maxHeat;
     }
 
     // Update is called once per frame
@@ -61,10 +77,67 @@ public class PlayerController : MonoBehaviour
             //Lock
             Cursor.lockState = CursorLockMode.Locked;
         }
+
+        // shooting:
+        if (!overHeat)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                shootCounter -= Time.deltaTime;
+                if (shootCounter <= 0)
+                {
+                    Shoot();
+                }
+            }
+            if (heatCounter <= 0)
+            {
+                heatCounter = 0f;
+            }
+            else
+            {
+                heatCounter -= coolRate * Time.deltaTime;
+            }
+        }
+        else
+        {
+            heatCounter -= overHeatCoolRate * Time.deltaTime;
+            if (heatCounter <= 0)
+            {
+                heatCounter = 0f;
+                overHeat = false;
+                UIController.instance.overHeatMessage.gameObject.SetActive(false);
+            }
+        }
+        UIController.instance.weaponHeatSlider.value = heatCounter;
     }
     private void LateUpdate()
     {
         cam.transform.position = viewPoint.position;
         cam.transform.rotation = viewPoint.rotation;
+    }
+
+    private void Shoot()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+        ray.origin = cam.transform.position;
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Debug.Log("Hitting: " + hit.collider.gameObject.name);
+            Destroy(Instantiate(bulletImpact, hit.point + (hit.normal * 0.002f), Quaternion.LookRotation(hit.normal, Vector3.up)), 10f);
+        }
+        shootCounter = timeBetweenShoots;
+        heatCounter += heatPerShoot;
+        if (heatCounter >= maxHeat)
+        {
+            heatCounter = maxHeat;
+            overHeat = true;
+
+            UIController.instance.overHeatMessage.gameObject.SetActive(true);
+        }
     }
 }
